792094c1cfa746c16adeada14b0099be5614792c
iIonel <masterxfightme@gmail.com>
2023-03-07 16:58:47 +0200

    .

--- lab3C/src/Main.java
+++ lab3C/src/Main.java
@@ -6,10 +6,16 @@ public class Main {
         Person Dani = new Person("Dani Mocanu");
         Person Florin = new Person("Florin Salam");
         Person Tzanca = new Person("Tzanca Uraganu");
+        Company UrziSoft = new Company("UrziSoft");
+        Company Smecheria = new Company("Scmecheria2000");
+        Company Fabrica = new Company("Fabrica de compot Arnold Schwarzenegger");
 
         nodes.add(Dani);
         nodes.add(Florin);
         nodes.add(Tzanca);
+        nodes.add(UrziSoft);
+        nodes.add(Smecheria);
+        nodes.add(Fabrica);
 
         System.out.println(nodes);
 
